740445b916ad103264b4de9b2e9513fc148afe93
Abhishek kalme <abhishekkalme0@gmail.com>
2025-04-20 23:56:30 +0530

    Add If-else-if statement example and input/output example in C++

---
+++ Learn_The_Basic/Things_to_Know_in_C++/Switch_Case_Statements.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int day = 7;
+    switch (day)
+    {
+    case 1:
+        cout << "Monday";
+        break;
+    case 2:
+        cout << "Tuesday";
+        break;
+    case 3:
+        cout << "wednesday";
+        break;
+    case 4:
+        cout << "Friday";
+        break;
+    case 5:
+        cout << "saturday";
+        break;
+    case 6:
+        cout << "sunday";
+        break;
+    default:
+    cout <<" enter a no";
+    }
+
+    return 0;
+}
