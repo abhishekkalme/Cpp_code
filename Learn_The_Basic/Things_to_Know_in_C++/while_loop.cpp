77218a004038526532b666c02ee93f43027f270d
Abhishek kalme <abhishekkalme0@gmail.com>
2025-04-22 23:56:32 +0530

    Refactor while loop example and add for loop examples in C++

--- Learn_The_Basic/Things_to_Know_in_C++/while_loop.cpp
+++ Learn_The_Basic/Things_to_Know_in_C++/while_loop.cpp
@@ -1,16 +1,44 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n = 5;
+//     int factorial = 1;
+
+//     while (n > 0) {
+//         factorial *= n;
+//         n--;
+//     }
+
+//     cout << "Factorial of 5 is: " << factorial << endl;
+
+//     return 0;
+// 
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    int n = 5;
-    int factorial = 1;
+    int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int target = 6;
 
-    while (n > 0) {
-        factorial *= n;
-        n--;
+    // Using 'break' to exit the loop early when the target is found
+    for (int num : numbers) {
+        if (num == target) {
+            cout << "Target found: " << target << endl;
+            break; // Exit the loop immediately
+        }
+        cout << "Checking: " << num << endl;
     }
 
-    cout << "Factorial of 5 is: " << factorial << endl;
+    // Using 'continue' to skip specific elements
+    for (int num : numbers) {
+        if (num % 2 == 0) {
+            continue; // Skip even numbers
+        }
+        cout << "Odd number: " << num << endl;
+    }
 
     return 0;
 }
+
