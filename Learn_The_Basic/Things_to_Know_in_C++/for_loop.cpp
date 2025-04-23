f3b9159f85110a863b32af707d68ec12ce73f04d
Abhishek kalme <abhishekkalme0@gmail.com>
2025-04-23 22:19:41 +0530

    Refactor for loop example and add nested for loop in C++

--- Learn_The_Basic/Things_to_Know_in_C++/for_loop.cpp
+++ Learn_The_Basic/Things_to_Know_in_C++/for_loop.cpp
@@ -1,9 +1,24 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     for (int i = 1; i <= 10; i++) {
+//         cout << "Hey, this is the " << i << "'th iteration" << endl;
+//     }
+//     return 0;
+// }
+
+
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    for (int i = 1; i <= 10; i++) {
-        cout << "Hey, this is the " << i << "'th iteration" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << "i = " << i << ", j = " << j << endl;
+            // Nested loop code
+        }
     }
     return 0;
 }
